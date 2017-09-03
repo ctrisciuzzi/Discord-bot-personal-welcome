bc56bc63532d65ed744359d4ceb742203a0bd8bd
ctrisciuzzi <carloalbertotrisciuzzi@gmail.com>
2017-09-03 15:21:03 +0200

    Update bot.js
    Correzione indentazione

--- bot.js
+++ bot.js
@@ -5,13 +5,13 @@ bot.on('message', message => {
     if (message.content.startsWith('ping')) {
         message.channel.sendMessage(message.author.id);
         /*
-		var voiceChannel = message.member.voiceChannel;
+        var voiceChannel = message.member.voiceChannel;
         voiceChannel.join()
 			.then(connection => {
-				const dispatcher = connection.playFile('C:/Users/Stefano/Documents/Levante.mp3');  
+                const dispatcher = connection.playFile('C:/Users/Stefano/Documents/Levante.mp3');
             })
             .catch(err => console.log(err));
-		*/
+        */
         message.channel.sendMessage(message.author);
     }
 })
@@ -21,39 +21,39 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => { //voiceStateUpdate: chang
 
 
     if (oldUserChannel === undefined && newUserChannel !== undefined) { //Membro entra nel canale
-		if (newMember.id==272401991165149185) { //se è Stefalber
-			var newMemberChannel =  newMember.voiceChannel;
-			newMemberChannel.join()
-			.then(connection => {
-				const dispatcher = connection.playFile('D:/AudioBot/veskim2.ogg');
-			})
-			.catch(err => console.log(err));
+        if (newMember.id==272401991165149185) { //se è Stefalber
+            var newMemberChannel =  newMember.voiceChannel;
+            newMemberChannel.join()
+            .then(connection => {
+                const dispatcher = connection.playFile('D:/AudioBot/veskim2.ogg');
+            })
+            .catch(err => console.log(err));
         } else if (newMember.id==126384828793749504) { //se è Veskim
             var newMemberChannel =  newMember.voiceChannel;
             newMemberChannel.join()
             .then(connection => {
-				const dispatcher = connection.playFile('D:/AudioBot/veskim3.ogg');
-			})
+                const dispatcher = connection.playFile('D:/AudioBot/veskim3.ogg');
+            })
             .catch(err => console.log(err));
         } else { //se è un tizio a caso
             var newMemberChannel =  newMember.voiceChannel;
             newMemberChannel.join()
             .then(connection => {
                 const dispatcher = connection.playFile('D:/AudioBot/veskim1.ogg');
-			})
+            })
             .catch(err => console.log(err));
         }
     } else if (newUserChannel === undefined) {
         var oldMemberChannel = oldMember.voiceChannel;
         oldMemberChannel.join()
-		.then(connection => {
+        .then(connection => {
             const dispatcher1 = connection.playFile('D:/AudioBot/veskim2.ogg');    
         })
         .catch(err => console.log(err));
     } else if (newUserChannel.id == 352561236707377194) { //se è già nel server ed entra in General
         var newMemberChannel = newMember.voiceChannel;
         newMemberChannel.join()
-		.then(connection => {
+        .then(connection => {
             const dispatcher1 = connection.playFile('D:/AudioBot/veskim1.ogg');
         })
         .catch(err => console.log(err));
