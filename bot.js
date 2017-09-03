d9bb24b0c50ef8c00a45ee7ebac9a7ac97ec8118
ctrisciuzzi <carloalbertotrisciuzzi@gmail.com>
2017-09-03 15:02:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,23 +12,23 @@ bot.on('message', message => {
             })
             .catch(err => console.log(err));
 		*/
-            message.channel.sendMessage(message.author)
+            message.channel.sendMessage(message.author);
     }
 })
 bot.on('voiceStateUpdate', (oldMember, newMember) => { //voiceStateUpdate: change channel, mute/unmute
-    let newUserChannel = newMember.voiceChannel
-    let oldUserChannel = oldMember.voiceChannel
+    let newUserChannel = newMember.voiceChannel;
+    let oldUserChannel = oldMember.voiceChannel;
 
 
     if (oldUserChannel === undefined && newUserChannel !== undefined) { //Membro entra nel canale
-		if (newMember.id==272401991165149185){ //se è Stefalber
+		if (newMember.id==272401991165149185) { //se è Stefalber
 			var newMemberChannel =  newMember.voiceChannel;
 			newMemberChannel.join()
 			.then(connection => {
 				const dispatcher = connection.playFile('D:/AudioBot/veskim2.ogg');
 			})
 			.catch(err => console.log(err));
-        } else if (newMember.id==126384828793749504){ //se è Veskim
+        } else if (newMember.id==126384828793749504) { //se è Veskim
             var newMemberChannel =  newMember.voiceChannel;
             newMemberChannel.join().then(connection => {
 				const dispatcher = connection.playFile('D:/AudioBot/veskim3.ogg');
@@ -39,16 +39,14 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => { //voiceStateUpdate: chang
               const dispatcher = connection.playFile('D:/AudioBot/veskim1.ogg');
 			})
         }
-    } else if (newUserChannel === undefined){
+    } else if (newUserChannel === undefined) {
         var oldMemberChannel = oldMember.voiceChannel;
         oldMemberChannel.join()
 		.then(connection => {
             const dispatcher1 = connection.playFile('D:/AudioBot/veskim2.ogg');    
         })
         .catch(err => console.log(err));
-    
-
-    } else if (newUserChannel.id == 352561236707377194){ //se è già nel server ed entra in General
+    } else if (newUserChannel.id == 352561236707377194) { //se è già nel server ed entra in General
         var newMemberChannel = newMember.voiceChannel;
         newMemberChannel.join()
 		.then(connection => {
